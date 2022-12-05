bfa7ffdf68cc3c64d4594d2203f98b7f763186da
samar ben hadj abbes <samarabbes3@gmail.com>
2022-12-05 09:18:26 +0100

    second commit

---
+++ app/src/main/java/com/samar/car_manager/MainActivity2.java
@@ -0,0 +1,14 @@
+package com.samar.car_manager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
